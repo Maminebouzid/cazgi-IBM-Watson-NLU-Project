ef4b98176cabe9bd11d225e52c9b003aff391073
Maminebouzid <52084905+Maminebouzid@users.noreply.github.com>
2021-08-22 00:04:40 +0100

    adding getNLUInstance

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = new express();
-
+const dotenv = require('dotenv');
+dotenv.config();
 app.use(express.static('client'))
-
+function getNLUInstance()
+{
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_KEY;
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+     const { IamAuthenticator } = require('ibm-watson/auth');
+     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: '2021-08-01',
+  authenticator: new IamAuthenticator({
+    apikey: api_key,
+  }),
+  serviceUrl: api_url,
+});
+return naturalLanguageUnderstanding;
+}
 const cors_app = require('cors');
 app.use(cors_app());
 
